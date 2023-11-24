6812a3eeef5cc5960be0e4e1a9b59952a99892bc
unknown <vladadzhun@gmail.com>
2023-11-24 13:30:31 +0200

    add styles

--- src/View/NewsView.php
+++ src/View/NewsView.php
@@ -61,30 +61,37 @@ class NewsView {
         <head>
             <title>Створення новини</title>
             <link rel="stylesheet" href="/static/css/app.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         </head>
         <body>
             <div class="container">
-                <h1  class = "title-news">Створення новини</h1>
-                
-                <?php echo $message; ?>
-                <div class="create-news-container">
-                    <form method="post" action="index.php?action=create_news">
-                        <label class = "news-title">Заголовок:</label>
-                        <input type="text" name="title" required><br>
-                    
-                        <label class = "news-text">Текст:</label>
-                        <textarea name="text" rows="4" required></textarea><br>
-                    
-                        <button type="submit" class = "create-news-button">Створити новину</button>
-                    </form>
-                
-                    <p><a href="index.php?action=home" class = "return-button">На головну</a></p>
+                <div class="wrapper wrapper--lg">
+
+                    <h1  class = "title title-md">Створення новини</h1>
+                    <?php echo $message; ?>
+                    <div class="form-wrapper">
+                        <form method="post" action="index.php?action=create_news">
+                            <label class = "form-item__label">Заголовок:</label>
+                            <input class = "form-item__field" type="text" name="title" required><br>
+
+                            <div class="form-item">
+                            <label class = "form-item__label">Текст:</label>
+                            <textarea name="text" rows="4" required></textarea><br>
+                            </div>
+                        
+                            <div class="form-footer">
+                                <button type="submit" class = "btn btn-primary">Створити новину</button>
+                                <p><a href="index.php?action=home" class = "return-button">На головну</a></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </body>
         </html>
 
-                <?php
+    <?php
             
     }
 
@@ -115,24 +122,31 @@ class NewsView {
         <head>
             <title>Редагування новини</title>
             <link rel="stylesheet" href="/static/css/app.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         </head>
         <body>
             <div class="container">
-                <h1 class = "edit_comment_news">Редагування новини</h1>
+                <h1 class = "title title-md">Редагування новини</h1>
                 
                 <?php echo $message; ?>
                 <div class="create-news-container">
                     <form method="post" action="index.php?action=edit_news&news_id=<?php echo $newsId; ?>">
-                        <label class = "label-news-title">Заголовок:</label>
-                        <input type="text" name="title" value="<?php echo $news['title']; ?>" required><br>
-                        
-                        <label class = "text_edit_comment_news">Текст:</label>
-                        <textarea name="text" rows="4" required><?php echo $news['text']; ?></textarea><br>
-                        
-                        <button type="submit" class = "button_edit_comment_news">Зберегти зміни</button>
+                        <div class="form-item">
+                            <label class = "form-item__label">Заголовок:</label>
+                            <input class="form-item__field" type="text" name="title" value="<?php echo $news['title']; ?>" required><br>
+                        </div>
+
+                        <div class="form-item">
+                            <label class = "form-item__label">Текст:</label>
+                            <textarea name="text" rows="4" required><?php echo $news['text']; ?></textarea><br>
+                        </div>
+
+                        <div class="form-footer">
+                            <button type="submit" class = "btn btn-primary">Зберегти зміни</button>
+                            <p><a href="index.php?action=home" class = "return-button">На головну</a></p>
+                        </div>
                     </form>
-                    
-                    <p><a href="index.php?action=home" class = "return-button">На головну</a></p>
                 </div>
             </div>
         </body>
@@ -143,13 +157,13 @@ class NewsView {
 
     public function renderComments($commentList,$userModel, $message = '') {
         ?>
-        <h2 class = "title-comment">Коментарі</h2>
+        <h2 class = "title title-md">Коментарі</h2>
         <?php
         if (!empty($commentList)) {
             foreach ($commentList as $comment) {
                 echo '<div>';
                 if ($userModel->isUserLoggedIn() && $userModel->isAuthor($_SESSION['user_id'], $comment['author_id'])){
-                echo '<div class="button-container">';
+                echo '<div class="comment-item">';
                 echo '<a href="index.php?action=edit_comment&comment_id='.$comment['id'].'" class = "edit-comment-button">Редагувати коментар </a>';
                 echo '<form method="post" action="index.php?action=delete_comment">';
                 echo '<input type="hidden" name="id" value='.$comment['id'].' required><br>';
@@ -179,22 +193,26 @@ class NewsView {
             <link rel="stylesheet" href="/static/css/app.css">
         </head>
         <body>
-            <h1>Створення коментаря</h1>
-            
-            <?php echo $message; ?>
-            <div class="create-comment-container">
-                <form method="post" action="index.php?action=create_comment&news_id=<?php echo $news_id; ?>">
-                    <label>Текст коментаря:</label>
-                    <textarea name="text" rows="4" required></textarea><br>
-                
-                    <button type="submit" class = "leave-comment-button">Залишити коментар</button>
-                </form>
-            
-                <p><a href="index.php?action=home" class = "return-button">На головну</a></p>
+            <div class="wrapper wrapper--lg">
+                <h1 class="title title-md">Створення коментаря</h1>
+                <?php echo $message; ?>
+                    <div class="form-wrapper">
+                        <form method="post" action="index.php?action=create_comment&news_id=<?php echo $news_id; ?>">
+                            <div class="form-item">
+                                <label class="form-item__label">Текст коментаря:</label>
+                                <textarea class="form-item__field" name="text" rows="4" required></textarea><br>
+                            </div>
+
+                            <div class="form-footer">
+                                <button type="submit" class = "btn btn-primary">Залишити коментар</button>
+                                <p><a href="index.php?action=home" class = "return-button">На головну</a></p>
+                            </div>
+                        </form>
+                    </div>
             </div>
         </body>
         </html>
-                <?php
+<?php
     }
 
     public function updateComments($commentsId, $comment, $message) {
